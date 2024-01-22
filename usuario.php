6947d5e74ce3c612f3704ce86f0c27b75c3b2bcf
macichonek <57502961+macichonek@users.noreply.github.com>
2024-01-22 09:30:13 -0300

    Add files via upload

---
+++ usuario.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="author" content="Tiago Panis Macichonek">
+    <meta name="description" content="Projeto desenvolvido para conclusão da cadeira de projeto aplicado - Escolas e Faculdades QI.">
+    <title>Monitor de Itinerários</title>
+  </head>
+
+  <body>
+    <?php
+
+    include 'diasuteis.php';
+
+    ?>
+  </body>
+</html>
+
+<!--Após aceito login, chama a página diasuteis.php-->
